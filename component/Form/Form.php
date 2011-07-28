fe1842074d268f0db9add4376bbb0cb5d5115ca6
Ephigenia <love@ephigenia.de>
2011-07-28 11:48:19 +0200

    * moved one step forward for comicblog.net by re-arranging the locations of libraries and added ephframe as module

--- component/Form/Form.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace app\component\Form;
-
-class Form extends \ephFrame\HTML\Form\Form
-{
-}
--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,24 +4,23 @@ namespace app;
 
 define('COMPILE_START', microtime(true));
 define('APP_ROOT', realpath(dirname(__DIR__)));
+define('LIB_ROOT', APP_ROOT.'/lib');
+
+defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
 
 // intitialize ephFrame 0.3 with it's libs and stuff
-if (getenv('APPLICATION_ENV') == 'ephigenia') {
-	define('EPHFRAME_PATH', '/Users/ephigenia/Sites/ephFrame/trunc_0.3/vendor/ephFrame');
-} else {
-	define('EPHFRAME_PATH', '/kunden/277171_10245/ephFrame_0.3');
-}
+define('EPHFRAME_PATH', LIB_ROOT.'/ephFrame/lib/ephFrame');
 if (!include EPHFRAME_PATH.'/core/Library.php') {
-	$message =
+	$message = 
 		'ephFrame core could not be found. Check the value of EPHFRAME_PATH in '.
 	 	'config/bootstrap.php. It should point to the directory containing your '.
 		'ephFrame directory.';
-	trigger_error($message, E_USER_ERROR);
+	die(trigger_error($message, E_USER_ERROR));
 }
 
 \ephFrame\core\Library::add('ephFrame', EPHFRAME_PATH);
-\ephFrame\core\Library::add('app', APP_ROOT);
-\ephFrame\core\Library::add('harrison', APP_ROOT.'/vendor/harrison');
+\ephFrame\core\Library::add('harrison', LIB_ROOT.'/harrison');
+\ephFrame\core\Library::add('app',		LIB_ROOT.'/app');
 
 require __DIR__.'/bootstrap/doctrine.php';
 require __DIR__.'/config.php';
